248dc74d7ff1e6ba87b83dcc95b3dabd137f473b
Michał Smyk <msmyk2@softserveinc.com>
2023-05-14 23:36:49 +0200

    Initial commit

---
+++ DotNet/src/ExampleWebApp.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace ExampleWebApp.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Assert.Fail();
+        }
+    }
+}
